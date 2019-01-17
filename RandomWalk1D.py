bc7f9ef6b84b133ae4a0e34affcfd96894eb56fc
jasstmuley <trupti1510014@nitp.ac.in>
2019-01-17 12:26:46 +0530

    average found

--- RandomWalk1D.py
+++ RandomWalk1D.py
@@ -10,22 +10,24 @@ import matplotlib.pyplot as plt
 
 #number of particles
 N = 100
-itrs = 100
-x = np.zeros((N,itrs))
+itrs = 1000000
+x = np.zeros((itrs,N))
+average = [0]
 
-##function for movement
-#def move(temp):
-#    if random() <= 0.5:
-#        return temp + 1
-#    else: return temp -1
+#function for movement
+def move(temp):
+    if random() <= 0.5:
+        return temp + 1
+    else: return temp -1
 
 for i in range(1,itrs):
     for j in range(N):
-        if random() >= 0.5:
-            x[j][i] = x[j][i-1] - 1
-        else:
-            x[j][i] = x[j][i-1] + 1
+        x[i][j] = move(x[i-1][j])
+    average.append(sum(x[i])/N)
+    
         
-#plotting individual trajectories
-plt.plot(x[0])
-plt.plot(x[1])
+##plotting individual trajectories
+#plt.plot(x[0])
+    
+#plotting average
+plt.plot(average)
